05e04feed0812aeaa17687e26b4aa5d5195f49ea
Nithvin <nithvinlee@gmail.com>
2025-05-31 22:36:00 +0800

    Update CORS

--- src/main/java/com/party_personality/party_personality/config/CorsConfig.java
+++ src/main/java/com/party_personality/party_personality/config/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Apply to all endpoints
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "https://nightlyaffair.github.io") // Your React app URLs
+                .allowedOrigins("http://localhost:3000",
+                                "http://localhost:3001",
+                                "https://nightlyaffair.github.io",
+                                "https://pookies-creations.github.io") // Your React app URLs
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
