455a0c9a3fad9f92a8d59d2a39e4a546ed9228d3
Nithvin <nithvinlee@gmail.com>
2025-06-01 11:31:39 +0800

    Local Development Tools

--- frontend/src/data/Data.js
+++ frontend/src/data/Data.js
@@ -7,6 +7,9 @@ async function Data() {
         const apiData = await APIFetch();
         if (apiData) {
             return ParseJson(apiData);
+        } else {
+            //Try local fetch
+            return ParseJson(LocalFetch());
         }
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -29,6 +32,20 @@ async function APIFetch() {
     } catch (error) {
         console.error('API fetch error:', error);
         throw error;
+    }
+}
+
+//Mostly only for local development
+async function LocalFetch() {
+    try {
+        const localData = await fetch("./data.json");
+        if (!localData) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await localData.json();
+        return json;
+    } catch (error) {
+        console.error("No local data found", error);
     }
 }
 
